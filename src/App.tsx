300e8488ee22dafcf167efb78e8ed056c996c0e9
Android-JCPP-Servo <astewart1138@gmail.com>
2023-05-17 13:39:15 -0500

    Exercise: Showing an Alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
+
+  const [alertVisible, setAlertVisible] = useState(false);
+
   let items = ["New York", "San Francisco", "Austin", "Chicago", "Watsonville"];
 
   const handleSelectItem = (item: string) => {
@@ -11,15 +15,13 @@ function App() {
 
   return (
     <div>
-      <Alert>
-        Hello World<span>!</span>
-      </Alert>
+      {alertVisible && <Alert onClose={() => setAlertVisible(false)}>Hello World<span>!</span></Alert>}
       <ListGroup
         items={items}
         heading={"Cities"}
         onSelectItem={handleSelectItem}
       ></ListGroup>
-      <Button onClick={() => console.log("Hello!")}>React Button</Button>
+      <Button onClick={() => setAlertVisible(true)}>React Button</Button>
     </div>
   );
 }
